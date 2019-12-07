b998b37a7c7a5864e022277479be2db78d5fa809
alexismayfire <calexandrelvieira@gmail.com>
2019-12-07 18:49:46 -0300

    Correções no trabalho final Algumas alterações na função de identificação com a máscara Removidos comentários e código que não é mais utilizado

--- trabalho_final/main.py
+++ trabalho_final/main.py
@@ -9,9 +9,9 @@ LEFT_LINE = None
 RIGHT_LINE = None
 BOTTOM_LINE = None
 TIME_COUNTER = 1
-BOTTOM_MASK_LINE = [(385,160),(385,320)]
-BOTTOM_MASK_LINE = [(265,320),(375,320)]
+LEFT_MASK_LINE = [(255,160),(255,320)]
 RIGHT_MASK_LINE = [(385,160),(385,320)]
+BOTTOM_MASK_LINE = [(265,320),(375,320)]
 
 
 class Mask:
@@ -38,29 +38,14 @@ class Mask:
         self.alpha_s = self.s_img[:, :, 0] / 255
    
     def _square_shape(self):  # desenha a forma de 3 retas no meio da tela
-        # Linha da esquerda
-        cv2.line(
-            self.frame, (self.x, self.y), (self.x, self.height - self.y), (255, 0, 0), 3
-        )
-        LEFT_MASK_LINE = [(self.width - self.x, self.y), (self.width - self.x, self.height - self.y)]
-        # print(LEFT_MASK_LINE)
-        # Linha da direita
-        cv2.line(
-            self.frame, (self.width - self.x, self.y), (self.width - self.x, self.height - self.y),(255, 0, 0), 3,
-        )
-        RIGHT_MASK_LINE = [(self.width - self.x, self.y), (self.width - self.x, self.height - self.y)]
-        print(self.width - self.x, self.y,self.width - self.x, self.height - self.y)
-        # Linha inferior
-        cv2.line(
-            self.frame,
-            (self.x + 10, self.height - self.y),
-            (self.width - self.x - 10, self.height - self.y),
-            (255, 0, 0),
-            3,
-        )
-        BOTTOM_MASK_LINE = [(self.x + 10, self.height - self.y),(self.width - self.x - 10, self.height - self.y)]
-        # print(BOTTOM_MASK_LINE[0][1])
-    
+        color = (255, 0, 0)
+        left_start, left_end = (255, 160), (255, 320)
+        right_start, right_end = (385, 160), (385, 320)
+        bottom_start, bottom_end = (265, 320), (375, 320)
+
+        cv2.line(self.frame, left_start, left_end, color, 3)
+        cv2.line(self.frame, right_start, right_end, color, 3)
+        cv2.line(self.frame, bottom_start, bottom_end, color, 3)
 
     def _detect_square(self, deviation_allowed=15):
         # TODO: por enquanto só detecta linhas horizontais e verticais, cortando
@@ -84,15 +69,33 @@ class Mask:
                     # uma das linhas verticais)
 
                     # LINHA DA ESQUERDA
-                    if x1 < 320 or x2 < 320:
-                        (self.x, self.y), (self.x, self.height - self.y)
-                        dif_x = abs(x1 - self.x)
+                    left_x = 255
+                    vertical_lower_y = 160
+                    vertical_upper_y = 320
+                    l_lower_x = left_x - deviation_allowed
+                    l_upper_x = left_x + deviation_allowed
+
+                    right_x = 385
+                    r_lower_x = right_x - deviation_allowed
+                    r_upper_x = right_x + deviation_allowed
+
+                    if (l_lower_x < x1 < l_upper_x) or (l_lower_x < x2 < l_upper_x):
+                        # (self.x, self.y), (self.x, self.height - self.y)
+
+                        dif_x1 = abs(x1 - left_x)
+                        dif_x2 = abs(x2 - left_x)
+
+                        # Pega a menor diferença, caso a linha esteja ligeiramente inclinada
+                        if dif_x1 > dif_x2:
+                            dif_x = dif_x2
+                        else:
+                            dif_x = dif_x1
 
                         # Identifica se está próximo da linha do centro da esquerda?
                         if y1 > y2:
-                            dif_y = abs(y2 - self.y) + abs(y1 - (self.height - self.y))
+                            dif_y = abs(y2 - vertical_lower_y) + abs(y1 - vertical_upper_y)
                         else:
-                            dif_y = abs(y1 - self.y) + abs(y2 - (self.height - self.y))
+                            dif_y = abs(y1 - vertical_lower_y) + abs(y2 - vertical_upper_y)
 
                         # O quanto pode estar distante da linha de marcação na tela (Tolerância)
                         tolerance_y = 25
@@ -104,19 +107,26 @@ class Mask:
                             and self.left_line is None
                         ):
                             self.left_line = [(x1, y1), (x2, y2)]
-
                         else:
                             cv2.line(self.frame, (x1, y1), (x2, y2), (0, 0, 255), 3)
-                    else:
+
+                    elif (r_lower_x < x1 < r_upper_x) or (r_lower_x < x2 < r_upper_x):
                         # LINHA DA DIREITA
-                        (RIGHT_MASK_LINE[0][0], self.y), (RIGHT_MASK_LINE[0][0], self.height - self.y)
-                        dif_x = abs(x1 - RIGHT_MASK_LINE[0][0])
+                        # (RIGHT_MASK_LINE[0][0], self.y), (RIGHT_MASK_LINE[0][0], self.height - self.y)
+                        dif_x1 = abs(x1 - right_x)
+                        dif_x2 = abs(x2 - right_x)
+
+                        # Pega a menor diferença, caso a linha esteja ligeiramente inclinada
+                        if dif_x1 > dif_x2:
+                            dif_x = dif_x2
+                        else:
+                            dif_x = dif_x1
 
                         # Identifica se está próximo da linha do centro da direita
                         if y1 > y2:
-                            dif_y = abs(y2 - self.y) + abs(y1 - (self.height - self.y))
+                            dif_y = abs(y2 - vertical_lower_y) + abs(y1 - vertical_upper_y)
                         else:
-                            dif_y = abs(y1 - self.y) + abs(y2 - (self.height - self.y))
+                            dif_y = abs(y1 - vertical_lower_y) + abs(y2 - vertical_upper_y)
 
                         # O quanto pode estar distante da linha de marcação na tela (Tolerância)
                         tolerance_y = 25
@@ -136,88 +146,72 @@ class Mask:
                 elif deviation_allowed > abs(y1 - y2) and deviation_allowed < abs(
                     x1 - x2
                 ):
+                    target_y = 320 
+                    horizontal_lower_x = 265
+                    horizontal_upper_x = 375
+                
+                    target_lower_y = target_y - deviation_allowed
+                    target_upper_y = target_y + deviation_allowed
 
-                    #print('Linha Horizontal:', x1,y1,':',x2,y2)
-                    #cv2.line(self.frame, (x1, y1), (x2, y2), (0, 250, 160), 3)
-                    
-                    # O quanto pode estar distante da linha de marcação na tela (Tolerância)
-                    tolerance_y = 25
-                    tolerance_x = 25
+                    if (target_lower_y < y1 < target_upper_y) or (target_lower_y < y2 < target_upper_y):
+                        dif_y1 = abs(y1 - target_y)
+                        dif_y2 = abs(y2 - target_y)
 
-                    # cv2.line(self.frame, (x1, y1), (x2, y2), (0, 0, 255), 3)
+                        # Pega a menor diferença, caso a linha esteja ligeiramente inclinada
+                        if dif_y1 > dif_y2:
+                            dif_y = dif_y2
+                        else:
+                            dif_y = dif_y1
 
-                    # Encontrou uma linha próxima a bottom
-                    if abs(y1 - BOTTOM_MASK_LINE[0][1]) < tolerance_y:                       
-                        if self.bottom_line is None:
-                            print(y1,'-',BOTTOM_MASK_LINE[0][1],'=',abs(y1 - BOTTOM_MASK_LINE[0][1]) )
+                        # Identifica se está próximo da linha do centro da esquerda?
+                        if x1 > x2:
+                            dif_x = abs(x2 - horizontal_lower_x) + abs(x1 - horizontal_upper_x)
+                        else:
+                            dif_x = abs(x1 - horizontal_lower_x) + abs(x2 - horizontal_upper_x)
+
+                        # O quanto pode estar distante da linha de marcação na tela (Tolerância)
+                        tolerance_y = 25
+                        tolerance_x = 25
+
+                        if (
+                            tolerance_y > dif_y
+                            and tolerance_x > dif_x
+                            and self.bottom_line is None
+                        ):
                             self.bottom_line = [(x1, y1), (x2, y2)]
-                            cv2.line(self.frame, (x1, y1), (x2, y2), (0, 250, 160), 3)
-
-                    # Identifica se está próximo da linha de baixo (Por algum motivo bizarro, BOTTOM_MASK_LINE sempre pega a global inicialmente definida)
-                    ''' O Y2 está maluco
-                    if y1 > y2:
-                        dif_y = abs(y2 - BOTTOM_MASK_LINE[0][1]) + abs(y1 - (self.height - BOTTOM_MASK_LINE[0][1]))
-                        #print(y2,'-',BOTTOM_MASK_LINE[0][1],'=',abs(y2 - BOTTOM_MASK_LINE[0][1]) )
-                        print('abs(y1 - 320):', abs(y1 - BOTTOM_MASK_LINE[0][1]),',abs(y2 - 320):',abs(y2 - (self.height - BOTTOM_MASK_LINE[0][1])), ' dif:', dif_y)
-                    else:
-                        dif_y = abs(y1 - BOTTOM_MASK_LINE[0][1]) + abs(y2 - (self.height - BOTTOM_MASK_LINE[0][1]))
-                        #print(y1,'-',BOTTOM_MASK_LINE[0][1],'=',abs(y1 - BOTTOM_MASK_LINE[0][1]) )
-                        print('abs(y1 - 320):', abs(y1 - BOTTOM_MASK_LINE[0][1]),',abs(y2 - 320):',abs(y2 - (self.height - BOTTOM_MASK_LINE[0][1])), ' dif:', dif_y)
-
-                    if (
-                        tolerance_y > dif_y
-                        and tolerance_x > dif_x
-                        and self.bottom is None
-                    ):
-                        self.bottom_line = [(x1, y1), (x2, y2)]
-
-                    else:
-                        cv2.line(self.frame, (x1, y1), (x2, y2), (0, 0, 255), 3)
-            
-                    '''
-                    if y1 > y2:  # Linha vindo de CIMA ESQUERDA para BAIXO DIREITA
-                        dif_y = abs(y2 - BOTTOM_MASK_LINE[0][1]) + abs(y1 - (self.height - BOTTOM_MASK_LINE[0][1]))
-                    else:  # Linha vindo de CIMA DIREITA para BAIXO ESQUERDA
-                        dif_y = abs(y1 - BOTTOM_MASK_LINE[0][1]) + abs(y2 - (self.height - BOTTOM_MASK_LINE[0][1]))
-                    
-
-                    if (
-                        tolerance_y > dif_y
-                        and tolerance_x > dif_x
-                        and self.bottom_line is None
-                    ):
-                        self.bottom_line = [(x1, y1), (x2, y2)]
+                        else:
+                            cv2.line(self.frame, (x1, y1), (x2, y2), (0, 0, 255), 3)
                 
 
     def _draw_tattoo(self):
         if self.left_line and self.right_line and self.bottom_line:
             # Left Line Params
-            x1, y1 = self.left_line[0]
-            x2, y2 = self.left_line[1]
-            cv2.line(
-                self.frame, (x1, y1), (x2, y2), (0, 250, 160), 3
-            )  # Yellow | Linha Amarela
+            (x1, y1), (x2, y2) = self.left_line
+            cv2.line(self.frame, (x1, y1), (x2, y2), (0, 250, 160), 3)  # Yellow | Linha Amarela
 
             # Right Line Params
-            x1_right_line, y1_right_line = self.right_line[0]
-            x2_right_line, y2_right_line = self.left_line[1]
-            cv2.line(
-                self.frame, (x1_right_line, y1_right_line), (x2_right_line, y2_right_line), (0, 250, 160), 3
-            )  # Yellow | Linha Amarela
+            (x1, y1), (x2, y2) = self.right_line
+            cv2.line(self.frame, (x1, y1), (x2, y2), (0, 250, 160), 3)  # Yellow | Linha Amarela
 
             # Bottom Line Params
-            x1_bottom_line, y1_bottom_line = self.bottom_line[0]
-            x2_bottom_line, y2_bottom_line = self.bottom_line[1]
-            cv2.line(
-                self.frame, (x1_bottom_line, y1_bottom_line), (x2_bottom_line, y2_bottom_line), (0, 250, 160), 3
-            )  # Yellow | Linha Amarela
+            (x1, y1), (x2, y2) = self.bottom_line
+            cv2.line(self.frame, (x1, y1), (x2, y2), (0, 250, 160), 3)  # Yellow | Linha Amarela
 
-            # Posicionamento contando apenas com a linha da esquerda
-            x_offset_aux = x1
-            y_offset_aux = y1 - self.s_img.shape[1]
+            (x1, y1), (x2, y2) = self.left_line
+            x_left = (x1 + x2) // 2 # Valor médio do X da linha da esquerda
+            y_left = (y1 + y2) // 2 # Valor médio do Y da linha da esquerda
 
-            y1_aux, y2_aux = (y_offset_aux, y_offset_aux + self.s_img.shape[0])
-            x1_aux, x2_aux = (x_offset_aux, x_offset_aux + self.s_img.shape[1])
+            (x1, y1), (x2, y2) = self.right_line
+            x_right = (x1 + x2) // 2 # Valor médio do X da linha da direita
+            y_right = (y1 + y2) // 2 # Valor médio do Y da linha da direita
+        
+            (x1, y1), (x2, y2) = self.bottom_line
+
+            y_lower = y_right if y_right > y_left else y_left
+            y_upper = (y1 + y2) // 2 # Valor médio do Y na linha de baixo
+
+            y1_aux, y2_aux = (y_lower, y_upper)
+            x1_aux, x2_aux = (x_left, x_right)
 
             # for c in range(0, 2):
             # self.frame[y1_aux:y2_aux, x1_aux:x2_aux] = (self.alpha_s * self.s_img[:, :] + self.alpha_l * self.frame[y1_aux:y2_aux, x1_aux:x2_aux])
@@ -227,28 +221,6 @@ class Mask:
                         self.frame[y, x, 0] = self.alpha_s[y2, x2]
                         self.frame[y, x, 1] = self.alpha_s[y2, x2]
                         self.frame[y, x, 2] = self.alpha_s[y2, x2]
-    '''
-    def _x_shape(self):
-        # Linha da esquerda do X
-        cv2.line(
-            self.frame,
-            (self.x, self.y),
-            (self.width - self.x, self.height - self.y),
-            (255, 0, 0),
-            3,
-        )
-        # Linha da direita do X
-        cv2.line(
-            self.frame,
-            (self.width - self.x, self.y),
-            (self.x, self.height - self.y),
-            (255, 0, 0),
-            3,
-        )
-
-    def _detect_x(self):
-        pass
-    '''
 
     def detect(self):
         if self.shape == SQUARE:
@@ -261,16 +233,11 @@ class Mask:
 
         return (self.frame, self.left_line, self.right_line, self.bottom_line)
 
-'''
-def detect_x_shape(frame, line):
-    x1, y1, x2, y2 = line[0]
-'''
-
 
 def hough(frame, edges, format=SQUARE):
     # Aqui tentando identificar aquele mesmo símbolo do InkHunter: |_|
     # Thresh acima de 100 ja dificulta pra encontrar
-    thresh = 60
+    thresh = 40
     # Esse valor aparentemente é fixo no InkHunter, se você não encaixar
     # certinho na sobreposição do símbolo não acha.
     # Então precisa definir um valor bacana aqui pra ajudar no Hough!
@@ -285,6 +252,27 @@ def hough(frame, edges, format=SQUARE):
     )
 
     return lines
+
+
+def hsv_blue(frame):
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    background = np.zeros(frame.shape, dtype=np.uint8)
+    sensitivity = 20
+    lower_range = np.array(
+        [110 - sensitivity, 50, 50]
+    )  # Range de cores analisadas (limite inferior)
+    upper_range = np.array(
+        [130 + sensitivity, 255, 255]
+    )  # Range de cores analisadas (limite inferior)
+    mask = cv2.inRange(hsv, lower_range, upper_range)
+    mask_3d = np.zeros(frame.shape, dtype=np.uint8)
+    for ch in range(2):
+        mask_3d[:, :, ch] = mask[:, :]
+    pre_edges = np.where(mask_3d == 0, background, frame)
+
+    return pre_edges
+
 
 if __name__ == "__main__":
     cap = cv2.VideoCapture(0)
@@ -307,21 +295,7 @@ if __name__ == "__main__":
         # gray = np.zeros((frame.shape[0], frame.shape[1]), dtype=np.uint8)
         # gray[:, :] = np.where(mask == 0, 0, 255)
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-        background = np.zeros(frame.shape, dtype=np.uint8)
-        sensitivity = 20
-        lower_range = np.array(
-            [110 - sensitivity, 50, 50]
-        )  # Range de cores analisadas (limite inferior)
-        upper_range = np.array(
-            [130 + sensitivity, 255, 255]
-        )  # Range de cores analisadas (limite inferior)
-        mask = cv2.inRange(hsv, lower_range, upper_range)
-        mask_3d = np.zeros(frame.shape, dtype=np.uint8)
-        for ch in range(2):
-            mask_3d[:, :, ch] = mask[:, :]
-        pre_edges = np.where(mask_3d == 0, background, frame)
+        pre_edges = hsv_blue(frame)
 
         edges = cv2.Canny(pre_edges, 80, 200)
         kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
@@ -354,7 +328,11 @@ if __name__ == "__main__":
             RIGHT_LINE = None
             BOTTOM_LINE = None
 
-        print(left_line, right_line, bottom_line)
+        print('---------------')
+        print(f'LEFT: {left_line}')
+        print(f'RIGHT: {right_line}')
+        print(f'BOTTOM:{bottom_line}')
+        print('---------------')
 
         height, width, channels = frame.shape
         side = np.zeros((height, width * 2, channels), dtype=np.uint8)
